fc5103d7c9f50dd848d2be5cbf517794908791ba
sweater_weather <kireevasvetlana2004@gmail.com>
2023-02-28 10:16:46 +0300

    labar2

---
+++ pr1.py
@@ -0,0 +1,42 @@
+# 1
+# password = int(input("Введите пароль: "))
+# password1 = int(input("Подтвердите пароль: "))
+# if password == password1:
+#   print ("Пароль принят")
+# else:
+#    print ("Пароль не принят")
+
+#2
+# typee = ""
+# nom = int(input("Введите номер места"))
+# if nom <= 54:
+#     if 0 < nom < 37:
+#         typee = "Купе"
+#     else:
+#         typee = "Боковое"
+#     if nom % 2 == 0:
+#         typee += " верх"
+#     else:
+#         typee += " низ"
+# else:
+#     print("Неправильный номер")
+# print(typee)
+
+#3
+# year = int(input("Введите год"))
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0 ):
+#     print ("Год", year, "- високосный")
+# else:
+#     print ("Этот год не високосный")
+
+#4
+# color = input("Введите цвет")
+# color2 = input("Введите цвет")
+# if color == ("желтый" and color2 == "красный") or (color == "красный" and color2 == "желтый"):
+#     print("оранжевый")
+# elif (color == "желтый" and color2 == "синий") or (color == "синий" and color2 == "желтый"):
+#     print("зеленый")
+# elif (color == "красный" and color2 == "синий") or (color == "синий" and color2 == "красный"):
+#     print ("фиолетовый")
+# else:
+#     print("Введите правильные цвета!!!")
